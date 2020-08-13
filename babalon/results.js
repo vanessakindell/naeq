4de9efc531f8c25510d6774baab5f7fea27f0863
Vanessa Kindell <vanessakindell@gmail.com>
2020-08-12 23:58:26 -0400

    Added Daughter of Fortitude

--- babalon/results.js
+++ babalon/results.js
@@ -14,6 +14,65 @@ input.oninput = function() {
 
 	    result.innerHTML = a;
 
+	    /* Fortitude */
+	    textset = fortitude.split(" ");
+        matchescount = 0;
+        outputmatch = "";
+        teststr = "";
+        testword = "";
+        testchar = "";
+        teststrval = 0;
+        testweight = 0;
+        if(a!==0) {
+            for(textpos=0;textpos<textset.length;textpos++) {
+                teststr = "";
+                teststrval = 0;
+                testword = "";
+                if(textpos+20>textset.length) {
+                    offend = textset.length;
+                } else {
+                    offend = textpos+20;
+                }
+                for(offset=textpos;offset<offend;offset++) {
+                    testweight = 0;
+                    testword = textset[offset];
+                    for(subpos=0;subpos<testword.length;subpos++) {
+                        testchar = testword.substring(subpos,subpos+1);
+                        if(typeof cipher[testchar] !== 'undefined') {
+                            testweight = parseInt(testweight) + parseInt(cipher[testchar]);
+                        } else if(testchar == parseInt(testchar)) {
+                            testweight = parseInt(testweight) + parseInt(testchar);
+                        }
+                    }
+                    if(parseInt(testword)==testword)
+                    {
+                      testweight = parseInt(testword);
+                    }
+                    teststrval = parseInt(teststrval) + parseInt(testweight);
+                    if(teststr)
+                    {
+                      teststr = teststr + " " + testword;
+                    }
+                    else
+                    {
+                      teststr = testword;
+                    }
+                    if(teststrval==a) {
+                        matchescount++;
+                        outputmatch += "<li>"+teststr+"</li>";
+                    }
+                }
+            }
+        }
+	    if (matchescount !== 0) {
+	        fortcount.innerHTML = matchescount;
+	        fortmatch.innerHTML = outputmatch;
+
+	    } else {
+            fortcount.innerHTML = 0;
+	        fortmatch.innerHTML = "N/A";
+	    }
+
 	    /* XLIX */
 	    textset = liberxlix.split(" ");
         matchescount = 0;
@@ -69,7 +128,7 @@ input.oninput = function() {
 	        xlixmatch.innerHTML = outputmatch;
 
 	    } else {
-            xlixcount.innerHTML = 0;
+          xlixcount.innerHTML = 0;
 	        xlixmatch.innerHTML = "N/A";
 	    }
   }, 666);
